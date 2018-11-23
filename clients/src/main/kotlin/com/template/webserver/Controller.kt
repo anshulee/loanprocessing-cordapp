f6946a8e1edba15b143d3e8bed8530127eb1ec0d
Anshulee <anshulee@cennest.com>
2018-11-23 16:15:36 +0530

    added server logic

--- clients/src/main/kotlin/com/template/webserver/Controller.kt
+++ clients/src/main/kotlin/com/template/webserver/Controller.kt
@@ -1,9 +1,15 @@
 package com.template.webserver
 
+import com.loannetwork.bank.RecieveApplication
+import com.loannetwork.base.model.LoanStateModel
+import net.corda.core.internal.randomOrNull
+import net.corda.core.messaging.startFlow
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.ws.rs.core.Response
+
 
 /**
  * Define your API endpoints here.
@@ -11,15 +17,29 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/") // The paths for HTTP requests are relative to this base path.
 class Controller(rpc: NodeRPCConnection) {
-
+    private val loanApplicationsList= mutableListOf<LoanStateModel>()
     companion object {
         private val logger = LoggerFactory.getLogger(RestController::class.java)
     }
 
     private val proxy = rpc.proxy
 
-    @GetMapping(value = "/templateendpoint", produces = arrayOf("text/plain"))
-    private fun templateendpoint(): String {
-        return "Define an endpoint here."
+    private fun generateData()
+    {
+        var recordCount=0
+        while(recordCount<100) {
+            recordCount= recordCount+1
+            val record = LoanStateModel("RECIEVED", applicant = recordCount.toString(), builderName = "BB", propertyName = "CC", address = "DD", loanAmount = 8000F, appliedDate = null, updatedDate = null)
+            loanApplicationsList.add(record)
+        }
+    }
+    @GetMapping(value = "/generateData", produces = arrayOf("application/json"))
+    private fun generateApplicationData():Response  {
+        generateData()
+      for( record in loanApplicationsList)
+      {
+            proxy.startFlow( ::RecieveApplication, record )
+        }
+        return Response.ok().build()
     }
 }
--- loannetwork-cordapp-states/src/main/kotlin/com/loannetwork/base/model/LoanStateModel.kt
+++ loannetwork-cordapp-states/src/main/kotlin/com/loannetwork/base/model/LoanStateModel.kt
@@ -2,7 +2,8 @@ package com.loannetwork.base.model
 
 import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.Party
+import net.corda.core.serialization.CordaSerializable
 import java.time.Instant
 import java.util.*
-
+@CordaSerializable
 data class LoanStateModel(val status: String, val applicant:String, val builderName:String, val propertyName:String, val address:String, val loanAmount:Float,  val appliedDate: Date?, val updatedDate: Date?)
